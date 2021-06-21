7a7b44972e6306ee3e7b97580c4457c4582e011b
Arka <mkharisma11@gmail.com>
2021-06-21 17:32:58 +0700

    Fixing currency bug

--- app/src/main/java/com/velxoz/finalproject/util/Currency.java
+++ app/src/main/java/com/velxoz/finalproject/util/Currency.java
@@ -4,11 +4,8 @@ import java.text.NumberFormat;
 import java.util.Locale;
 
 public class Currency {
-    private static Locale locale;
+    private static final Locale locale = new Locale("in", "ID");;
 
-    public Currency(){
-        locale = new Locale("in", "ID");
-    }
     public static String getCurrencyFormat(Integer number){
         NumberFormat formatter=NumberFormat.getCurrencyInstance(locale);
         return formatter.format(Double.valueOf(number));
